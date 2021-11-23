527e1f29f93542915aff5f406a8f593074489d87
chenminzhi1 <92850935+chenminzhi1@users.noreply.github.com>
2021-11-23 17:20:35 +0800

    a
    a

---
+++ ppp/src/main/java/entry/Test.java
@@ -0,0 +1,42 @@
+package entry;
+
+public class Test {
+	private int id;
+	private String name;
+	private String addr;
+
+	
+	public void aa() {
+		System.out.println("00000");
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+	@Override
+	public String toString() {
+		return "Test [id=" + id + ", name=" + name + ", addr=" + addr + "]";
+	}
+
+}
